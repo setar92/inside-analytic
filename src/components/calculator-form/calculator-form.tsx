a9f93cf9f28a8890babd15cef95a217a9c122713
setar92 <tarasiuks2303@gmail.com>
2023-06-23 13:56:13 +0300

    rendered markers on the calculator map

--- src/components/calculator-form/calculator-form.tsx
+++ src/components/calculator-form/calculator-form.tsx
@@ -1,5 +1,7 @@
-import { FC, useState, useRef } from 'react';
+import { FC, useState, useRef, useEffect } from 'react';
+import Geocode from 'react-geocode';
 import { FaLocationArrow, FaTimes } from 'react-icons/fa';
+import uuid from 'react-uuid';
 
 import {
   Box,
@@ -8,16 +10,20 @@ import {
   Flex,
   HStack,
   IconButton,
-  Input,
   Text,
 } from '@chakra-ui/react';
 import {
   useJsApiLoader,
   GoogleMap,
-  Marker,
-  Autocomplete,
   DirectionsRenderer,
 } from '@react-google-maps/api';
+
+import { allLocationsData } from '../../common/constants';
+import { CommonLocation, IAllLocationsData } from '../../common/types';
+import { MyMarker } from '../../components/marker/MyMarker';
+import { filterData } from '../../helpers/filter-logic';
+import { useAppSelector } from '../../hooks/store/store.hooks';
+import { defaultOptions } from '../map/options';
 
 const center = { lat: 56.940763, lng: 24.138074 };
 
@@ -29,12 +35,46 @@ const CalculatorForm: FC = () => {
 
   const originRef = useRef<HTMLInputElement>(null);
   const destiantionRef = useRef<HTMLInputElement>(null);
+  const [allLocations, setAllLocations] = useState<IAllLocationsData[]>([]);
 
   const [map, setMap] = useState<google.maps.Map | null>(null);
   const [directionsResponse, setDirectionsResponse] =
     useState<google.maps.DirectionsResult | null>(null);
   const [distance, setDistance] = useState('');
   const [duration, setDuration] = useState('');
+
+  Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAP as string);
+  Geocode.setLanguage('en');
+  Geocode.setRegion('es');
+  Geocode.setLocationType('ROOFTOP');
+  Geocode.enableDebug();
+
+  const filterCriterions = useAppSelector((state) => state.filter);
+  const geocodeFunction = async function (): Promise<void> {
+    const response = await Geocode.fromLatLng(
+      String(center.lat),
+      String(center.lng),
+    );
+    console.log(
+      response.results[0].formatted_address,
+      'response.results[0].formatted_address',
+    );
+  };
+  geocodeFunction();
+
+  useEffect(() => {
+    const locationsData = filterData(allLocationsData, filterCriterions);
+    setAllLocations(locationsData);
+  }, [filterCriterions]);
+
+  const choosePostMachineHandler = (location: CommonLocation): void => {
+    // showData(location);
+    // setPosition({
+    //   lat: location.latitude,
+    //   lng: location.longitude,
+    // });
+    console.log(location, 'location');
+  };
 
   async function calculateRoute(): Promise<void> {
     if (originRef.current?.value && destiantionRef.current?.value) {
@@ -45,6 +85,9 @@ const CalculatorForm: FC = () => {
         return;
       }
     }
+    setDirectionsResponse(null);
+    setDistance('');
+    setDuration('');
     // eslint-disable-next-line no-undef
     const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
@@ -78,52 +121,43 @@ const CalculatorForm: FC = () => {
     return <div>Hi</div>;
   }
   return (
-    <Flex className="relative flex-col align-middle h-full w-full">
+    <Flex className="relative flex-col align-middle h-full w-full mt-5 justify-center">
       <Box position="absolute" left={0} top={0} h="100%" w="100%">
         {/* Google Map Box */}
         <GoogleMap
           center={center}
           zoom={15}
-          mapContainerStyle={{ width: '100%', height: '500px' }}
-          options={{
-            zoomControl: false,
-            streetViewControl: false,
-            mapTypeControl: false,
-            fullscreenControl: false,
-          }}
+          mapContainerStyle={{ width: '100%', height: '650px' }}
+          options={defaultOptions}
           onLoad={(map): void => setMap(map)}
         >
-          <Marker position={center} />
+          {allLocations.map((locationData) => {
+            return locationData.data.map((loc) => {
+              console.log(
+                { lat: loc.latitude, lng: loc.longitude },
+                'we are hea',
+              );
+              return (
+                <MyMarker
+                  key={uuid()}
+                  position={{ lat: loc.latitude, lng: loc.longitude }}
+                  onClick={choosePostMachineHandler}
+                  allInfo={loc}
+                  iconUrl={locationData.marker}
+                />
+              );
+            });
+          })}
           {directionsResponse && (
             <DirectionsRenderer directions={directionsResponse} />
           )}
         </GoogleMap>
       </Box>
       <Box
-        className="p-2 rounded-xl m-2 bg-white shadow-sm"
-        minW="container.md"
+        className="p-4 rounded-xl m-2 bg-white shadow-sm w-[60%] ml-auto mr-auto"
         zIndex="1"
       >
         <HStack spacing={2} justifyContent="space-between">
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input
-                className="p-2"
-                type="text"
-                placeholder="Origin"
-                ref={originRef}
-              />
-            </Autocomplete>
-          </Box>
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input
-                type="text"
-                placeholder="Destination"
-                ref={destiantionRef}
-              />
-            </Autocomplete>
-          </Box>
           <ButtonGroup>
             <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
               Calculate Route
